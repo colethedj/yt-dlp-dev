7d6109fb22a0d10013eb7da449726f2fc0a644a1
coletdjnz <coletdjnz@protonmail.com>
2022-01-25 19:08:23 +1300

    misc ideas

---
+++ yt_dlp/network/adapter.py
@@ -0,0 +1,88 @@
+import abc
+import bisect
+import http.cookiejar
+
+import urllib.request
+import urllib.parse
+from abc import ABC, abstractmethod
+from collections import OrderedDict
+from typing import List
+
+
+class NotSuitableError(Exception):
+    ...
+
+
+class BaseBackendAdapter(ABC):
+
+    BACKEND_NAME: str
+    PRIORITY = 0  # highest priority
+    PROTOCOLS: list
+
+    def __init__(self, cookies: http.cookiejar.CookieJar, youtubedl_params: dict):
+        self.cookiejar = cookies
+        self.params = youtubedl_params
+        super(BaseBackendAdapter).__init__()
+
+    def request(self, request: urllib.request.Request, *req_args, **req_kwargs):
+        scheme = urllib.parse.urlparse(request.full_url).scheme
+        if scheme.lower() not in self.PROTOCOLS:
+            raise NotSuitableError(f'{scheme} is not a supported scheme')
+        return self._request(request, *req_args, **req_kwargs)
+
+    @abstractmethod
+    def _request(self, request: urllib.request.Request, proxies=None):
+        raise NotImplementedError('This function must be implemented by subclasses')
+
+    def __lt__(self, other):
+        return self.PRIORITY < other.PRIORITY
+
+
+class MyBackendAdapter(BaseBackendAdapter):
+    BACKEND_NAME = 'my backend'
+    PRIORITY = 40
+    SUPPORTED_METHODS = ['http', 'https']
+
+    def __init__(self, cookies: http.cookiejar.CookieJar, youtubedl_params: dict):
+        super().__init__(cookies, youtubedl_params)
+        # do some stuff
+
+    def _request(self, request: urllib.request.Request, proxies=None):
+        if proxies:
+            raise NotSuitableError
+        raise NotImplementedError
+
+
+class Session:
+
+    def __init__(self, youtubedl_params: dict, logger, proxies=None):
+        self._adapters: List[BaseBackendAdapter] = []
+        self.global_proxies = proxies or {}
+        self._logger = logger
+        self.params = youtubedl_params
+
+    def add_adapter(self, adapter):
+        bisect.bisect(self._adapters, adapter)
+
+    def remove_adapter(self, adapter: BaseBackendAdapter):
+        self._adapters.remove(adapter)
+
+    def send(self, request: urllib.request.Request):
+        for adapter in self._adapters:
+            try:
+                return adapter.request(request, proxies=self.global_proxies)
+            except NotSuitableError as e:
+                if str(e):
+                    self._logger.error(f'{adapter.BACKEND_NAME} backend could not be used: {e}')
+        raise Exception('No appropriate adapter available that can resolve this request')
+
+
+# goes in YoutubeDL class?
+def create_session(youtubedl_params, ydl_logger):
+    adapters = [MyBackendAdapter]
+    session = Session(youtubedl_params, logger=ydl_logger)
+    cookies = http.cookiejar.CookieJar()
+    for adapter in adapters:
+        if not adapter:
+            continue
+        session.add_adapter(adapter(cookies, youtubedl_params))
