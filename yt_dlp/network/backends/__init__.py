b518d34e3f558fd5218b69d40df64dc97251cf20
coletdjnz <coletdjnz@protonmail.com>
2022-02-10 10:57:15 +1300

    Use RequestError for unsupported backend handler

--- yt_dlp/network/backends/__init__.py
+++ yt_dlp/network/backends/__init__.py
@@ -1,10 +1,11 @@
 from ._urllib import UrllibHandler
 from ..common import YDLBackendHandler
+from ...exceptions import RequestError
 
 
 class UnsupportedBackendHandler(YDLBackendHandler):
     def can_handle(self, request, **req_kwargs):
-        raise Exception('This request is not supported')
+        raise RequestError('This request is not supported')
 
 
 __all__ = ['UrllibHandler', 'UnsupportedBackendHandler']
