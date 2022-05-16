1dad20b3c14ddf9d3b47f103fb482fc9a0aeb68e
coletdjnz <coletdjnz@protonmail.com>
2022-05-16 14:13:08 +1200

    Revert "Implement ALL_PROXY"
    This reverts commit 654c5c9621cb015b4c1e87ccb99690adbc79feb8.

--- yt_dlp/networking/utils.py
+++ yt_dlp/networking/utils.py
@@ -1,7 +1,6 @@
 import random
 import ssl
 import sys
-import urllib.parse
 
 from ..compat import compat_urlparse, compat_urllib_parse_unquote_plus
 from .socksproxy import ProxyType
@@ -129,15 +128,3 @@ def socks_create_proxy_args(socks_proxy):
         'username': unquote_if_non_empty(url_components.username),
         'password': unquote_if_non_empty(url_components.password),
     }
-
-
-def select_proxy(url, proxies):
-    """Unified proxy selector for all backends"""
-    if proxies is None:
-        proxies = {}
-    url_components = urllib.parse.urlparse(url)
-    priority = [
-        url_components.scheme or 'http',  # prioritise more specific mappings
-        'all'
-    ]
-    return next((proxies[key] for key in priority if key in proxies), None)
