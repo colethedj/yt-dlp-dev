3bc72edd3e0e1bd7e92259682699840134152d0e
coletdjnz <coletdjnz@protonmail.com>
2022-06-24 15:36:16 +1200

    remove redundant check

--- yt_dlp/networking/_requests.py
+++ yt_dlp/networking/_requests.py
@@ -212,7 +212,7 @@ class YDLRequestsSession(requests.sessions.Session):
         new_method = get_redirect_method(prepared_request.method, response.status_code)
 
         # HACK: requests removes headers/body on redirect unless code was a 307/308.
-        if not (new_method != prepared_request.method and new_method == 'GET'):
+        if not new_method != prepared_request.method:
             response._real_status_code = response.status_code
             response.status_code = 308
 
