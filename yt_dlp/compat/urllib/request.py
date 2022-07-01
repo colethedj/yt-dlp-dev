cf6b8311017a48393ef3ba7ed900e2c5a119a463
coletdjnz <coletdjnz@protonmail.com>
2022-07-01 14:59:26 +1200

    Only care about https proxies

--- yt_dlp/compat/urllib/request.py
+++ yt_dlp/compat/urllib/request.py
@@ -9,12 +9,12 @@ del passthrough_module
 
 
 if compat_os_name == 'nt':
-    # Workaround for https://github.com/python/cpython/issues/86793 for older python versions
-    # This issue only occurs for the case where there is only
-    # one proxy url for all protocols and if the proxy scheme is not http.
-    # urllib will erroneously set http, https and ftp proxy types
-    # to such proxy url with the proxy type scheme appended on with no checks.
-    # See: https://github.com/python/cpython/blob/51f1ae5ceb0673316c4e4b0175384e892e33cc6e/Lib/urllib/request.py#L2693-L2698
+    # Workaround for an issue on older python versions where proxies are extracted from Windows registry erroneously. [1]
+    # If the https proxy in the registry does not have a scheme, urllib will incorrectly add https:// to it. [2]
+    # It is unlikely that the user has set it to actually be https,
+    # so we should be fine to safely downgrade it to http on these affected versions to avoid issues.
+    # 1: https://github.com/python/cpython/issues/86793
+    # 2: https://github.com/python/cpython/blob/51f1ae5ceb0673316c4e4b0175384e892e33cc6e/Lib/urllib/request.py#L2683-L2698
     from urllib.request import getproxies_environment, _parse_proxy, getproxies_registry
 
     def getproxies_registry_patched():
@@ -25,19 +25,9 @@ if compat_os_name == 'nt':
         ):
             return proxies
 
-        # The erroneous branch will set all three of these
-        proxy_schemes = {'http', 'https', 'ftp'}
-        if set(proxies.keys()) != proxy_schemes and all(purl.startswith(f'{pk}://') for pk, purl in proxies.items()):
-            return proxies
+        if 'https' in proxies and proxies['https'].startswith(f'https://'):
+            proxies['https'] = 'http' + proxies['https'][5:]  # Downgrade https proxy to http
 
-        for proxy_key, proxy_url in proxies.items():
-            # Remove erroneously added proxy scheme and rebuild the proxy url
-            proxy_type, user, password, hp = _parse_proxy(proxy_url[len(f'{proxy_key}://'):])
-            # the existing proxy may or may not have a scheme. Use http as default.
-            proxies[proxy_key] = (proxy_type or 'http') + '://'
-            if user and password:
-                proxies[proxy_key] += f'{user}:{password}@'
-            proxies[proxy_key] += hp
         return proxies
 
     def getproxies():
