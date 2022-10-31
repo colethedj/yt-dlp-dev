fd4c5702873e0716a3d3566338797accab507024
coletdjnz <coletdjnz@protonmail.com>
2022-10-31 20:58:15 +1300

    template -> message

--- yt_dlp/extractor/knownunsupported.py
+++ yt_dlp/extractor/knownunsupported.py
@@ -7,14 +7,14 @@ from ..utils import classproperty
 class KnownUnsupportedBaseIE(InfoExtractor):
     IE_DESC = False  # Do not list
     UNSUPPORTED_SITES = ()
-    TEMPLATE = None
+    MESSAGE = None
 
     @classproperty
     def _VALID_URL(cls):
         return r'https?://(?:www\.)?(?:%s)' % '|'.join(rf'({s})' for s in cls.UNSUPPORTED_SITES)
 
     def _real_extract(self, url):
-        self.report_warning(self.TEMPLATE)
+        self.report_warning(self.MESSAGE)
         return self.url_result(url, 'Generic')
 
 
@@ -28,7 +28,7 @@ class KnownDRMIE(KnownUnsupportedBaseIE):
         r'peacocktv\.com',
         r'([a-zA-Z0-9_\.]+\.)?disneyplus\.com',
     )
-    TEMPLATE = (
+    MESSAGE = (
         'The requested site is known to use DRM protection. '
         'It WILL NOT be supported by yt-dlp, and will most likely fail to download. '
         'Please DO NOT open an issue, unless you have evidence that the video is not DRM protected'
